dffe3a3de9011c1fb71548a8a9e8b07b25544dbc
doraemon <57732767+deanakira@users.noreply.github.com>
2019-11-13 18:24:03 -0500

    Update SierpinskiTriangle.py

--- turtle/SierpinskiTriangle.py
+++ turtle/SierpinskiTriangle.py
@@ -1,1 +1,14 @@
+from turtle import *
+
+speed(2)
+def sierpinskiTriangle(length, depth):
+
+        
+
+	for i in range(3):
+		sierpinskiTriangle(length = length/2 , depth = depth-1)
+		forward (length)
+		right (120)
+		
+sierpinskiTriangle(500,7)
 
